104f0e7cabe96e7590bce0d71905f0abc0d9d638
Johel-code <jbsch16@gmail.com>
2024-12-22 21:45:36 -0400

    feat: add task controller with show all and show by id

--- src/main/java/com/task_manager/app/task/rest/controllers/TaskController.java
+++ src/main/java/com/task_manager/app/task/rest/controllers/TaskController.java
@@ -1,9 +1,17 @@
 package com.task_manager.app.task.rest.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.task_manager.app.task.rest.response.TaskResponse;
+import com.task_manager.domain.task.service.TaskService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,9 +19,20 @@ import lombok.RequiredArgsConstructor;
  * TaskController
  */
 @RequiredArgsConstructor
-@Validated
 @RestController
-@RequestMapping(path = "/v1/task")
+@RequestMapping(path = "/api/tasks")
 public class TaskController {
+
+  private final TaskService taskService;
+
+  @GetMapping("")
+  public ResponseEntity<List<TaskResponse>> showAll() {
+    return new ResponseEntity<>(taskService.showAll(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{taskId}")
+  public ResponseEntity<TaskResponse> show(@PathVariable Long taskId) {
+    return new ResponseEntity<>(taskService.show(taskId), HttpStatus.OK);
+  }
 
 }
